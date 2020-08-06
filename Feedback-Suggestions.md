fd4941adfb4e41663f25777ece56eb14edc0402d
Rachel Duchesneau <rachelduch95@gmail.com>
2020-08-05 22:22:29 -0500

    Add more notes

--- Feedback-Suggestions.md
+++ Feedback-Suggestions.md
@@ -4,3 +4,4 @@
 - Add Mutations
 - Add Powers
 - Add a toggle to swap between the alpha/beta and stable databases
+- Add DoT to traps (dps2)
--- src/screens/WhatsNew/AUGUST_05_2020.js
+++ src/screens/WhatsNew/AUGUST_05_2020.js
@@ -3,8 +3,8 @@ import React from "react";
 // <h4>August 5th 2020</h4>
 export default (
   <div>
-    <li>Added toggle to switch between beta and stable databases</li>
-    <li>I can't promise the new weapons have accurate DPS ;)</li>
+    <li>Added toggle to switch between beta and stable databases :D</li>
+    <li>Added cooldown to the combo section!</li>
     <br />
     <b>CAUTION: These weapons have INACCURATE DPS:</b>
     <li>Valmont's Whip</li>
@@ -13,5 +13,6 @@ export default (
     <li>Explosive Crossbow</li>
     <li>Boomerang</li>
     <li>Boy's Axe</li>
+    <li>Barrel Launcher</li>
   </div>
 );
