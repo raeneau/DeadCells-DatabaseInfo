a20c0b328dbd1dd1ed463e869bc7195b41c9f7ad
Rachel Duchesneau <rachelduch95@gmail.com>
2020-12-21 18:33:39 -0600

    Database is 2.1

--- src/constants/databaseVersion.js
+++ src/constants/databaseVersion.js
@@ -2,6 +2,6 @@ export const BETA = "DATABASE_BETA";
 
 export const STABLE = "DATABASE_STABLE";
 
-export const STABLE_VERSION = "2.0";
+export const STABLE_VERSION = "2.1";
 
 export const BETA_VERSION = "2.1";
