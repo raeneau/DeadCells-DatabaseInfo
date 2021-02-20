dd3c826a44cd6fd9a899de05c3787aa42b733ec7
Rachel Duchesneau <rachelduch95@gmail.com>
2021-02-20 14:54:36 -0600

    Wrong year :)

--- src/screens/WhatsNew/FEB_20_2021.js
+++ src/screens/WhatsNew/FEB_20_2021.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default (
   <div>
-    <h4>February 20, 2020</h4>
+    <h4>February 20, 2021</h4>
     <li>Added header that tells you what results the tool is displaying</li>
     <li>Added biomes to search</li>
     <li>Added enemies and scroll count to biome page</li>
